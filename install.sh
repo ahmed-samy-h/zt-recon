be6a6f33d802c7dbca090db552fb6ecd3ca6de8f
Ahmed Samy <ahmedsamyh27@gmail.com>
2026-07-11 10:30:36 +0300

    Fix Name Packg

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ echo -e "${GREEN}[+] Step 2: Installing Core Hacking Tools & Python dependencies
 apt update -y
 
 apt install -y nmap sqlmap dirsearch python3-pip python3-bs4 python3-rich wget unzip python3-full \
-    libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf2.0-0 libffi-dev shared-mime-info fonts-liberation
+    libpango-1.0-0 libpangocairo-1.0-0 libgdk-pixbuf-2.0-0 libffi-dev shared-mime-info fonts-liberation
 
 if [ -f "requirements.txt" ]; then
     pip3 install -r requirements.txt --break-system-packages 2>/dev/null
